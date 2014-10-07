3d3a9a0f99d22082accce79661a16014dc4dbe31
Anudeep <anudeep.mj@gmail.com>
2014-10-07 12:24:55 -0700

    commit 1

---
+++ src/chess/game.java
@@ -0,0 +1,32 @@
+package chess;
+
+public class game {
+	
+	public void play(){};
+	public void validateMove(){};
+	public void checkKills(){};
+	
+	Integer int1;
+	public void play1(Integer int1){
+		use(int1);
+	};
+	private void use(Integer int12) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] board = new int[7][7];
+		 
+//		void play(){ };
+//		public void validateMove(){};
+//		public checkKills();
+//		public isGameOver();
+//		
+
+	}
+
+}
